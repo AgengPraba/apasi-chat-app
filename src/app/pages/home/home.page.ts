949cb4470ade35ea67703ae1a0efbaced936d934
Ageng <agengpraba@gmail.com>
2024-11-18 07:58:23 +0700

    Refactor home page to integrate chat service and update user handling

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { PopoverController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { ChatService } from 'src/app/services/chat/chat.service';
 
 @Component({
   selector: 'app-home',
@@ -14,28 +15,45 @@ export class HomePage implements OnInit {
   @ViewChild('popover') popover: PopoverController;
   segment: string = 'chats';
   open_new_chat: boolean = false;
-  users = [
-    { id: 1, name: 'John Doe', photo: 'https://i.pravatar.cc/400?img=1' },
-    { id: 2, name: 'John Dono', photo: 'https://i.pravatar.cc/400?img=2' },
-    { id: 3, name: 'John Dani', photo: 'https://i.pravatar.cc/400?img=3' },
-  ];
+  users: Observable<any[]>;
+  // users = [
+  //   { id: 1, name: 'John Doe', photo: 'https://i.pravatar.cc/400?img=1' },
+  //   { id: 2, name: 'John Dono', photo: 'https://i.pravatar.cc/400?img=2' },
+  //   { id: 3, name: 'John Dani', photo: 'https://i.pravatar.cc/400?img=3' },
+  // ];
   chatRooms = [
     { id: 1, name: 'John Doe', photo: 'https://i.pravatar.cc/400?img=1' },
     { id: 2, name: 'John Dono', photo: 'https://i.pravatar.cc/400?img=2' },
     { id: 3, name: 'John Dani', photo: 'https://i.pravatar.cc/400?img=3' },
   ];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private chatService: ChatService) {}
 
   ngOnInit() {}
 
-  logout() {}
+  async logout() {
+    try {
+      console.log('logout');
+      this.popover.dismiss();
+      await this.chatService.auth.logout();
+      // this.chatService.currentUserId = null;
+      this.router.navigateByUrl('/login', { replaceUrl: true });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   onSegmentChanged(event: any) {
     this.segment = event.detail.value;
   }
   newChat() {
     this.open_new_chat = true;
+    if (!this.users) this.getUsers();
+  }
+
+  getUsers() {
+    this.chatService.getUsers();
+    this.users = this.chatService.users;
   }
 
   onWillDismiss(event: any) {}
--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api/api.service';
+import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
+  currentUserId: string;
+  public users: Observable<any>;
+  public chatRooms: Observable<any>;
+  public selectedChatRoomMessages: Observable<any>;
 
-  constructor() { }
+  constructor(public auth: AuthService, private api: ApiService) {
+    this.getId();
+  }
+
+  getId() {
+    console.log(this.currentUserId);
+    this.currentUserId = this.auth.getId();
+  }
+
+  getUsers() {
+    this.users = this.api.collectionDataQuery(
+      'users',
+      this.api.whereQuery('uid', '!=', this.currentUserId)
+    );
+  }
 }
